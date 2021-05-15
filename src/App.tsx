2a67e1430634cdb9b3c894b6b5696c07906aeaef
Richard Rohac <me@richardrohac.net>
2021-05-15 15:19:31 +0100

    Various fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import { Container, Divider, Grid, Icon, Header, Menu } from 'semantic-ui-react';
+import { Container, Divider, Grid, Header, Icon, Menu, Segment } from 'semantic-ui-react';
 import './App.css';
 import 'semantic-ui-css/semantic.min.css'
 
@@ -9,13 +9,13 @@ function App() {
   }, []);
 
   return (
-    <Container className='dark-container' fluid={true}>
+    <Container fluid={true}>
       <Container>
         <Menu inverted={true} pointing={true} secondary={true}>
           <Menu.Item name='Home' active={true}/>
         </Menu>
         <title>Richard Rohac | Home</title>
-        <Grid className='main-content'>
+        <Grid stacking={true} className='main-content'>
           <Grid.Row>
             <Grid.Column>
               <Header size='large' inverted={true}>Hey!</Header>
@@ -30,22 +30,22 @@ function App() {
           </Grid.Row>
           <Divider inverted={true}/>
           <Grid.Row>
-            <Grid.Column width={2}>
+            <Grid.Column width={3}>
               <Header size='medium' inverted={true}>
                 <a href='mailto:website@richardrohac.net'><Icon link={true} name='mail'/> e-mail</a>
               </Header>
             </Grid.Column>
-            <Grid.Column width={2}>
+            <Grid.Column width={3}>
               <Header size='medium' inverted={true}>
                 <a href='https://github.com/RichardRohac' target='_blank'><Icon link={true} name='github'/> GitHub</a>
               </Header>
             </Grid.Column>
-            <Grid.Column width={2}>
+            <Grid.Column width={3}>
               <Header size='medium' inverted={true}>
                 <a href='https://uk.linkedin.com/in/richard-rohac' target='_blank'><Icon link={true} name='linkedin'/> LinkedIn</a>
               </Header>
             </Grid.Column>
-            <Grid.Column width={2}>
+            <Grid.Column width={3}>
               <Header size='medium' inverted={true}>
                 <a href='https://steamcommunity.com/id/_theone_/' target='_blank'><Icon link={true} name='steam'/> Steam</a>
               </Header>
@@ -53,8 +53,7 @@ function App() {
           </Grid.Row>
         </Grid>
       </Container>
-      <Container className='footer centered secondary-text'>
-        <Divider inverted={true}/>
+      <Container className='footer-content secondary-text'>
         &#169; Richard Rohac | 2021
       </Container>
     </Container>
